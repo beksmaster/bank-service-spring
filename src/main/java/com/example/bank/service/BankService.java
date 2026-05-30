c6f19f5dd54f50be586ed1981f51077988ce0fac
beksmaster <beks.aaa@mail.ru>
2026-05-30 14:29:35 +0200

    feat: added deadlock-safe design, pessimistic locking

--- src/main/java/com/example/bank/service/BankService.java
+++ src/main/java/com/example/bank/service/BankService.java
@@ -22,16 +22,38 @@ public class BankService {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("Сумма должна быть больше нуля");
         }
-        Account fromAcc = accountRepository.findByNumberForUpdate(from)
-                .orElseThrow(() -> new IllegalArgumentException("Аккаунт отправителя не найден"));
 
-        Account toAcc = accountRepository.findByNumberForUpdate(to)
+        String first;
+        String second;
+        if(from.compareTo(to)< 0){
+            first = from;
+            second = to;
+        }else{
+            first = to;
+            second = from;
+        }
+
+        Account acc1 = accountRepository.findByNumberForUpdate(first) // блокируем через FOR UPDATE
+                .orElseThrow(() -> new IllegalArgumentException("Аккаунт отправителя не найден"));
+        Account acc2 = accountRepository.findByNumberForUpdate(second)
                 .orElseThrow(() -> new IllegalArgumentException("Аккаунт получателя не найден"));
+
+        Account fromAcc;
+        Account toAcc;
+
+        if(from.equals(first)){ // если аккаунты совпадают, то оставляем как есть.
+            fromAcc = acc1;
+            toAcc = acc2;
+        }else{
+            fromAcc = acc2; // если не совпадают то меняем между собой
+            toAcc = acc1;
+        }
 
 
         if (fromAcc.getBalance().compareTo(amount) < 0) {
             throw new IllegalArgumentException("Недостаточно средств");
         }
+
         fromAcc.setBalance(fromAcc.getBalance().subtract(amount));
         toAcc.setBalance(toAcc.getBalance().add(amount));
 
