8c8f59efb24c17e548f7517daf1570fc4b6dd54b
beksmaster <beks.aaa@mail.ru>
2026-06-22 23:08:29 +0600

    add flyway migration for account balance constraint

--- src/main/java/com/example/bank/BankApplication.java
+++ src/main/java/com/example/bank/BankApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankApplication {
 
-	public static void main(String[] args) {System.out.println("URL=" + System.getenv("DB_URL"));
-		System.out.println("USER=" + System.getenv("DB_USERNAME"));
-		System.out.println("PASS=" + System.getenv("DB_PASSWORD"));
+	public static void main(String[] args){
 		SpringApplication.run(BankApplication.class, args);
 	}
 
