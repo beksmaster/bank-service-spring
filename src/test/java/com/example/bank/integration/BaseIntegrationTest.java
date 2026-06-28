1966fba2999a5d81bfea5e68840a31c51461152a
beksmaster <beks.aaa@mail.ru>
2026-06-28 20:38:43 +0600

    TEST

--- src/test/java/com/example/bank/integration/BaseIntegrationTest.java
+++ src/test/java/com/example/bank/integration/BaseIntegrationTest.java
@@ -1,8 +1,9 @@
 package com.example.bank.integration;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -15,10 +16,28 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public abstract class BaseIntegrationTest {
 
     @Container
-    @ServiceConnection
     static PostgreSQLContainer<?> postgres =
             new PostgreSQLContainer<>("postgres:16-alpine")
                     .withDatabaseName("bank_test")
                     .withUsername("postgres")
-                    .withPassword("postgres");
+                    .withPassword("Bank2026");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+
+        registry.add(
+                "spring.datasource.url",
+                postgres::getJdbcUrl
+        );
+
+        registry.add(
+                "spring.datasource.username",
+                postgres::getUsername
+        );
+
+        registry.add(
+                "spring.datasource.password",
+                postgres::getPassword
+        );
+    }
 }
