c6fb38a1d566119a45bc79c0087ad34335ea7f8d
beksmaster <beks.aaa@mail.ru>
2026-06-30 19:02:10 +0600

    chore: remove public modifier

--- src/test/java/com/example/bank/integration/SecurityIntegrationTest.java
+++ src/test/java/com/example/bank/integration/SecurityIntegrationTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-public class SecurityIntegrationTest extends BaseIntegrationTest {
+ class SecurityIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     private TransactionRepository transactionRepository;
--- src/test/java/com/example/bank/service/TransactionsServiceTest.java
+++ src/test/java/com/example/bank/service/TransactionsServiceTest.java
@@ -24,7 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class TransactionsServiceTest {
+class TransactionsServiceTest {
     @Mock
     private TransactionRepository repository;
 
